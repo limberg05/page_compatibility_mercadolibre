e8e76ffd3bffc5a44ceacf48067ea97370e7e1b8
limberg05 <hernandezdelriojuan@gmail.com>
2024-10-11 02:05:45 -0600

    resolucion de problemas con config override

--- config-overrides.js
+++ config-overrides.js
@@ -1,10 +1,20 @@
+const webpack = require('webpack');
+
 module.exports = function override(config) {
   config.resolve.fallback = {
-    crypto: require.resolve('crypto-browserify'), // Necesario
-    stream: false, // No es necesario
-    buffer: false, // No es necesario
-    process: false, // Dejar a Webpack manejar process.env
+    crypto: require.resolve('crypto-browserify'),
+    stream: require.resolve('stream-browserify'),
+    buffer: require.resolve('buffer/'),
+    process: require.resolve('process/browser.js'), // Especificar la extensión .js
+    vm: false,
   };
+
+  config.plugins = (config.plugins || []).concat([
+    new webpack.ProvidePlugin({
+      process: 'process/browser.js', // Proveer process con la extensión .js
+      Buffer: ['buffer', 'Buffer'],
+    }),
+  ]);
 
   return config;
 };
